489627c06d7a9fcbcc0e0cf73c5a8ea82653915c
hukka-mail-ru <hukka@mail.ru>
2018-02-15 11:19:40 +0300

    dd

--- engine/game.py
+++ engine/game.py
@@ -1,27 +1,73 @@
-
+from string import ascii_lowercase
+import random
+import string
 
 class Game():
     
+    def getChipheredWord(self, word, cipher):
+    
+        chiphered = ""
+    
+        for w in word:
+            chiphered += cipher[w]
+            
+        return chiphered
+     
+    def getChipher(self, letters):
+        ciphered = {}
+        
+        for l in letters:
+            
+            cifer = ""
+            for i in range(0, 3):
+                cifer += random.choice(string.ascii_letters)
+             
+            ciphered[l] = cifer
+
+    
+        return ciphered
+    
+    def getLetters(self, word):
+        
+        distinct = {}
+        for w in word:
+            distinct[w] = 0
+        
+        letters = []
+        for key in distinct:
+            letters.append(key)
+    
+        return letters
+    
+    
     def chooseWord(self):
         
-        return "Hello"
+        return "hello"
     
     
     def start(self):
         
-        secretWord = self.chooseWord()
+        word = self.chooseWord()
+        letters = self.getLetters(word)
+        cipher = self.getChipher(letters)
         
-        print("chosen word", secretWord)
+        print("cipher", cipher)
+        
+        chiphered = self.getChipheredWord(word, cipher)
+        
+        print("ciphered word", chiphered)
         
         guessed = input('input something!: ') 
         
         print("guessed", guessed)
         
-        if (secretWord == guessed):
+        if (word == guessed):
             print("YES!")
         else:
             print("NO!")
 
 if __name__ == '__main__': # pragma: no cover
     game = Game()
-    game.start()    
+    
+    #game.setCipher()  
+    game.start()  
